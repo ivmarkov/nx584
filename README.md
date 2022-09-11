62e7a06efe1b58886a839abab297950f88c22c1c
imarkov <imarkov@vmware.com>
2022-09-11 20:02:26 +0300

    configuration

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-# NX584 Home Assistant Add-on
+# NX584 Home Assistant Add-On
 
-This is a very simple Home Assistant add-on for the [NX584 Interface Server](https://github.com/kk7ds/pynx584)
+This is a Home Assistant Add-On for the [NX584 Interface Server](https://github.com/kk7ds/pynx584)
 
-## How to use
+## How to use (from Home Assistant UI)
 
-* Fork this repository (this is necesary, because currently this add-on does not have any user configuration)
-* Refer to the [configuration documentation](https://github.com/kk7ds/pynx584) of the NX584 server and:
-  * Change [config.ini](nx584/config.ini) to describe your NX panel configuration (number of zones, zone names, ES vs US date-time format, etc.)
-  * Change [run.sh](nx584/run.sh) to reflect how you'll connect the NX584 server to the NX pannel. (Where it says `--connect 192.168.30.1:10000`)
-* Push your changes to your forked repository
-* Add your forked repository as a [Third-Party Add-On in Home Assistant](https://www.home-assistant.io/common-tasks/os#installing-third-party-add-ons)
-* [Configure your NX584 Alarm Panel and Binary Sensors](https://www.home-assistant.io/integrations/nx584/) in Home Assistant
+* Add this repo ([https://github.com/ivmarkov/nx584](https://github.com/ivmarkov/nx584)) as a [Third-Party Add-On](https://www.home-assistant.io/common-tasks/os#installing-third-party-add-ons)
+* Populate the Add-On Configuration page to reflect your setup, as described in the [pynx584 configuration documentation](https://github.com/kk7ds/pynx584)
+* [Configure your NX584 Alarm Panel and Binary Sensors](https://www.home-assistant.io/integrations/nx584/) (Interface server in the Add-On runs on the default `127.0.0.1:5007` host/port so you don't need to specify these)
 
 **HINT**: If the add-on was installed successfully, you should see something like this in the `Logs` section of the add-on:
 ```
--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,91 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.30.1:10000 --config /usr/app/src/config.ini
+
+CONFIG_PATH=/data/options.json
+
+CONNECTION="$(bashio::config 'connection')"
+DEVICE="$(bashio::config 'serial_device')"
+BAUDRATE="$(bashio::config 'serial_device_baudrate')"
+HOST="$(bashio::config 'host')"
+PORT="$(bashio::config 'port')"
+
+BINARY_PROTOCOL="$(bashio::config 'binary_protocol')"
+EU_DATE_FORMAT="$(bashio::config 'eu_date_format')"
+HEARTBEAT_SECS="$(bashio::config 'heartbeat_secs')"
+UPDATE_ZONE_NAMES="$(bashio::config 'update_zone_names')"
+ZONES="$(bashio::config 'zones')"
+ZONE_1_NAME="$(bashio::config 'zone_1_name')"
+ZONE_2_NAME="$(bashio::config 'zone_2_name')"
+ZONE_3_NAME="$(bashio::config 'zone_3_name')"
+ZONE_4_NAME="$(bashio::config 'zone_4_name')"
+ZONE_5_NAME="$(bashio::config 'zone_5_name')"
+ZONE_6_NAME="$(bashio::config 'zone_6_name')"
+ZONE_7_NAME="$(bashio::config 'zone_7_name')"
+ZONE_8_NAME="$(bashio::config 'zone_8_name')"
+ZONE_9_NAME="$(bashio::config 'zone_9_name')"
+ZONE_10_NAME="$(bashio::config 'zone_10_name')"
+ZONE_11_NAME="$(bashio::config 'zone_11_name')"
+ZONE_12_NAME="$(bashio::config 'zone_12_name')"
+ZONE_13_NAME="$(bashio::config 'zone_13_name')"
+ZONE_14_NAME="$(bashio::config 'zone_14_name')"
+ZONE_15_NAME="$(bashio::config 'zone_15_name')"
+ZONE_16_NAME="$(bashio::config 'zone_16_name')"
+
+if [$CONNECTION -eq "TCP/IP"]
+then
+  CONNECT="--connect $HOST:$PORT"
+else
+  CONNECT="--serial $DEVICE --baudrate $BAUDRATE"
+fi
+
+mkdirs /usr/app/src
+cat >/usr/app/src/config.ini <<EOF
+[config]
+# max_zone is the highest numbered zone you have populated
+max_zone = $ZONES
+
+# Disable zone name updates from NX8 system. If your system doesn't set zone
+# names and you prefer define them through pynx584, set this value to False.
+# Defaults to True
+zone_name_update = $UPDATE_ZONE_NAMES
+
+# Set to true if your unit sends DD/MM dates instead of MM/DD
+euro_date_format = $EU_DATE_FORMAT
+
+# Set To true if using binary protocol
+# Defaults to False
+use_binary_protocol = $BINARY_PROTOCOL
+
+# Length of idle time before sending heartbeat keep alive
+# (This is useful with the NX590 as it disconnects idle connections in a
+# short period of time)
+# Defaults to 120 seconds
+idle_time_heartbeat_seconds = $HEARTBEAT_SECS
+
+[zones]
+# Zone names
+1 = $ZONE_1_NAME
+2 = $ZONE_2_NAME
+3 = $ZONE_3_NAME
+4 = $ZONE_4_NAME
+5 = $ZONE_5_NAME
+6 = $ZONE_6_NAME
+7 = $ZONE_7_NAME
+8 = $ZONE_8_NAME
+9 = $ZONE_9_NAME
+10 = $ZONE_10_NAME
+11 = $ZONE_11_NAME
+12 = $ZONE_12_NAME
+13 = $ZONE_13_NAME
+14 = $ZONE_14_NAME
+15 = $ZONE_15_NAME
+16 = $ZONE_16_NAME
+EOF
+
+CONFIG_INI=$(cat /usr/app/src/config.ini)
+
+echo "About to run the NX584 server with setup:"
+echo ================\nConnection arg:\n$CONNECT\n"
+echo ================\nconfig.ini:\n$CONFIG_INI\n"
+echo ================\n"
+
+python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 $CONNECT --config /usr/app/src/config.ini
