b5b2200f0bb46eba65e51a52de379846a6e3317f
juanjoSanz <juanjo.sanzmartin@gmail.com>
2023-10-19 14:09:15 +0200

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -3,9 +3,8 @@
 
 bashio::log.info "Preparing to start..."
 
-## Get variables from the user config options.
-serial_dev=$(bashio::config 'serial_dev')
-serial_baud=$(bashio::config 'serial_baud')
+listen_addr=$(bashio::config 'listen_address')
+
 
 DEBUG=""
 if bashio::config.true 'debug_enabled'; then
@@ -13,8 +12,18 @@ if bashio::config.true 'debug_enabled'; then
   DEBUG="--debug "
 fi
 
-## Start Serial Interface
-python3 /usr/bin/nx584_server  --serial $serial_dev --baud $serial_baud --listen 0.0.0.0 --port 5007 --config /data/config.ini $DEBUG
+
+# Serial
+if bashio::config.true 'serial.enabled'; then
+    bashio::log.info "Serial enabled"
+    ## Get variables from the user config options.
+    serial_dev=$(bashio::config 'serial.device')
+    serial_baud=$(bashio::config 'serial.baud')
+
+    ## Start Serial Interface
+    bashio::log.info "Starting NX584 Server on Serial interface..."
+    python3 /usr/bin/nx584_server  --serial $serial_dev --baud $serial_baud --listen $listen_addr --port 5007 --config /data/config.ini $DEBUG
+fi
 
 ## Web interface
 #python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.30.1:10000 --config /usr/app/src/config.ini
