69ce2872782e88506feafc3cf8a3e49259975a6d
juanjoSanz <juanjo.sanzmartin@gmail.com>
2023-10-19 12:21:11 +0200

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -3,6 +3,9 @@
 ## Get variables from the user config options.
 serial_dev=$(bashio::config 'serial_dev')
 serial_baud=$(bashio::config 'serial_baud')
+config_file=$(bashio::config 'config_file')
+
+echo $config_file > /usr/app/src/config.ini 
 
 ## Start Serial Interface
 python3 /usr/bin/nx584_server  --serial $serial_dev --baud $serial_baud --listen 0.0.0.0 --port 5007 --config /usr/app/src/config.ini 
