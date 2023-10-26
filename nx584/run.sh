3c3d93d38c4015baff797929c7a22d94c32e6b5c
juanjoSanz <juanjo.sanzmartin@gmail.com>
2023-10-26 16:36:44 +0200

    Update run.sh
    remove listener address parameter

--- nx584/run.sh
+++ nx584/run.sh
@@ -3,9 +3,6 @@
 bashio::log.info $(cat /etc/os-release)
 bashio::log.info "Preparing to start..."
 
-docker_ip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
-listen_addr=$(bashio::config 'listen_address')
-bashio::log.info "Docker IP $docker_ip / Listener Address $listen_addr "
 
 DEBUG=""
 if bashio::config.true 'debug_enabled'; then
@@ -26,16 +23,16 @@ if bashio::config.true 'serial.enabled'; then
 
     ## Start nx server using serial Interface
     bashio::log.info "Starting NX584 Server on Serial interface..."
-    python3 /usr/bin/nx584_server  --serial $serial_dev --baud $serial_baud --listen $listen_addr --port 5007 --config /data/config.ini $DEBUG
+    python3 /usr/bin/nx584_server  --listen 0.0.0.0 --port 5007 --serial $serial_dev --baud $serial_baud --config /data/config.ini $DEBUG
 
 elif bashio::config.true 'socat.enabled'; then
-  bashio::log.info "socat enabled"
-  ## Get variables from the user config options.
-  socat_host=$(bashio::config 'socat.host')
-  socat_port=$(bashio::config 'socat.port')
+    bashio::log.info "socat enabled"
+    ## Get variables from the user config options.
+    socat_host=$(bashio::config 'socat.host')
+    socat_port=$(bashio::config 'socat.port')
 
-  ## Start nx server using socat interface
-  python3 /usr/bin/nx584_server --listen $listen_addr --port 5007 --connect ${socat_host}:${socat_port} --config /usr/app/src/config.ini $DEBUG
+    ## Start nx server using socat interface
+    python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect ${socat_host}:${socat_port} --config /usr/app/src/config.ini $DEBUG
 fi
 
 
