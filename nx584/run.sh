5d9747ed9c6461d6e10d3a73d8c4eea0fd7a00ed
juanjoSanz <juanjo.sanzmartin@gmail.com>
2023-10-26 15:15:29 +0200

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -5,7 +5,6 @@ bashio::log.info "Preparing to start..."
 
 listen_addr=$(bashio::config 'listen_address')
 
-
 DEBUG=""
 if bashio::config.true 'debug_enabled'; then
   bashio::log.info " * DEBUG Mode ON..."
@@ -20,13 +19,22 @@ if bashio::config.true 'serial.enabled'; then
     serial_dev=$(bashio::config 'serial.device')
     serial_baud=$(bashio::config 'serial.baud')
 
-    ## Start Serial Interface
+    ## Start nx server using serial Interface
     bashio::log.info "Starting NX584 Server on Serial interface..."
     python3 /usr/bin/nx584_server  --serial $serial_dev --baud $serial_baud --listen $listen_addr --port 5007 --config /data/config.ini $DEBUG
+
+elif bashio::config.true 'socat.enabled'; then
+  bashio::log.info "socat enabled"
+  ## Get variables from the user config options.
+  socat_host=$(bashio::config 'socat.host')
+  socat_port=$(bashio::config 'socat.port')
+
+  ## Start nx server using socat interface
+  python3 /usr/bin/nx584_server --listen $listen_addr --port 5007 --connect ${socat_host}:${socat_port} --config /usr/app/src/config.ini $DEBUG
 fi
 
-## Web interface
-#python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.30.1:10000 --config /usr/app/src/config.ini
+
+
 
 
 
